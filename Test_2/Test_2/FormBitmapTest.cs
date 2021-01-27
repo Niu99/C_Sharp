ca08be556c06f884690f7f6e4f6b4d3530052211
NSC <33211328+Niu99@users.noreply.github.com>
2021-01-27 09:40:23 +0800

    Add files via upload

---
+++ Test_2/Test_2/FormBitmapTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_2
+{
+    public partial class FormBitmapTest : Form
+    {
+        public FormBitmapTest()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap image1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Retrieve the image.
+                image1 = new Bitmap(@"C:\Users\n1512\Pictures\Camera Roll\Back.png", true);
+
+                int x, y;
+
+                // Loop through the images pixels to reset color.
+                for (x = 0; x < image1.Width; x++)
+                {
+                    for (y = 0; y < image1.Height; y++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                        image1.SetPixel(x, y, newColor);
+                    }
+                }
+
+                // Set the PictureBox to display the image.
+                pictureBox1.Image = image1;
+
+                // Display the pixel format in Label1.
+                label1.Text = "Pixel format: " + image1.PixelFormat.ToString();
+
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+        }
+    }
+}
