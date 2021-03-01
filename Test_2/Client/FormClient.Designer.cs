e599dde640d4ec6504ae1aa20bdcea1d3809dfb7
NSC <n584681851@163.com>
2021-03-01 18:16:22 +0800

    Add files via upload

---
+++ Test_2/Client/FormClient.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Client
+{
+    partial class FormClient
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtCMsg = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(371, 363);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(125, 36);
+            this.btnSend.TabIndex = 15;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtCMsg
+            // 
+            this.txtCMsg.Location = new System.Drawing.Point(42, 369);
+            this.txtCMsg.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtCMsg.Name = "txtCMsg";
+            this.txtCMsg.Size = new System.Drawing.Size(271, 29);
+            this.txtCMsg.TabIndex = 14;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(42, 138);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(451, 183);
+            this.txtMsg.TabIndex = 13;
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(371, 44);
+            this.btnListen.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(125, 36);
+            this.btnListen.TabIndex = 12;
+            this.btnListen.Text = "启动";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(149, 68);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(164, 29);
+            this.txtPort.TabIndex = 11;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(149, 16);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(164, 29);
+            this.txtIP.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 19);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 340);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 19);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "聊天信息输入";
+            // 
+            // FormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 446);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtCMsg);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FormClient";
+            this.Text = "客户机端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtCMsg;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
