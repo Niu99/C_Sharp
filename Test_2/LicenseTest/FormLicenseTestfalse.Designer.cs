128b44d4bf994a660f2bb18a33ca9b4b8b30bc8a
NSC <n584681851@163.com>
2021-03-01 18:19:14 +0800

    Add files via upload

---
+++ Test_2/LicenseTest/FormLicenseTestfalse.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace LicenseTest
+{
+    partial class FormLicenseTestfalse
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbRegistInfo = new System.Windows.Forms.Label();
+            this.btnRegist = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbRegistInfo
+            // 
+            this.lbRegistInfo.AutoSize = true;
+            this.lbRegistInfo.Location = new System.Drawing.Point(260, 80);
+            this.lbRegistInfo.Name = "lbRegistInfo";
+            this.lbRegistInfo.Size = new System.Drawing.Size(41, 12);
+            this.lbRegistInfo.TabIndex = 0;
+            this.lbRegistInfo.Text = "label1";
+            // 
+            // btnRegist
+            // 
+            this.btnRegist.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnRegist.Location = new System.Drawing.Point(243, 220);
+            this.btnRegist.Name = "btnRegist";
+            this.btnRegist.Size = new System.Drawing.Size(94, 33);
+            this.btnRegist.TabIndex = 1;
+            this.btnRegist.Text = "注册";
+            this.btnRegist.UseVisualStyleBackColor = true;
+            this.btnRegist.Click += new System.EventHandler(this.btnRegist_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSave.Location = new System.Drawing.Point(414, 220);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 33);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // FormLicenseTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRegist);
+            this.Controls.Add(this.lbRegistInfo);
+            this.Name = "FormLicenseTest";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormLicenseTest_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbRegistInfo;
+        private System.Windows.Forms.Button btnRegist;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Test_2/Server/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtPORT = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Location = new System.Drawing.Point(502, 183);
+            this.btnSendMsg.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(125, 36);
+            this.btnSendMsg.TabIndex = 15;
+            this.btnSendMsg.Text = "发送";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            this.btnSendMsg.Click += new System.EventHandler(this.btnSendMsg_Click);
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(442, 115);
+            this.txtSendMsg.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(249, 29);
+            this.txtSendMsg.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 19);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "IP";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(5, 68);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(427, 151);
+            this.txtMsg.TabIndex = 11;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(502, 16);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(125, 36);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "启动";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtPORT
+            // 
+            this.txtPORT.Location = new System.Drawing.Point(307, 17);
+            this.txtPORT.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtPORT.Name = "txtPORT";
+            this.txtPORT.Size = new System.Drawing.Size(164, 29);
+            this.txtPORT.TabIndex = 9;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(43, 17);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(164, 29);
+            this.txtIP.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 19);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "聊天信息输入框";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 292);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSendMsg);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPORT);
+            this.Controls.Add(this.txtIP);
+            this.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "服务器端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendMsg;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtPORT;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
